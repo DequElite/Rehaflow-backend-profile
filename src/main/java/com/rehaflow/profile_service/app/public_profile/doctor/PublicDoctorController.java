9320ec4d033e02573338cc2e54f3ae36f07aa58f
DequElite <maxrognov23@gmail.com>
2026-04-15 14:31:15 +0300

    created search client grpc

--- src/main/java/com/rehaflow/profile_service/app/public_profile/doctor/PublicDoctorController.java
+++ src/main/java/com/rehaflow/profile_service/app/public_profile/doctor/PublicDoctorController.java
@@ -1,10 +1,13 @@
 package com.rehaflow.profile_service.app.public_profile.doctor;
 
+import com.rehaflow.profile_service.app.public_profile.doctor.dto.DoctorFilters;
 import com.rehaflow.profile_service.app.public_profile.doctor.dto.DoctorGraphDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,4 +19,14 @@ public class PublicDoctorController {
     public DoctorGraphDTO doctorById(@Argument String id) {
         return this.getDoctorByIdService.getDoctorById(id);
     }
+
+    /*@QueryMapping
+    public List<DoctorGraphDTO> publicSearch(
+            @Argument String search,
+            @Argument DoctorFilters filters
+            ) {
+
+    }*/
+            
+
 }
